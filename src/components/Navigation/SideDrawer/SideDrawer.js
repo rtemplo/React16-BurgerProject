ccd8dfa033dfaec4bcb5264a8f6670f803564478
Ray Templo <rtemplo@gmail.com>
2018-02-22 11:18:44 -0500

    side drawer (134)

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import classes from './SideDrawer.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxilary';
 
 const sideDrawer = (props) => {
 
-
   return (
-    <div></div>
+    <Aux>
+    <Backdrop show />
+    <div className={classes.SideDrawer}>
+      <div className={classes.Logo}>
+        <Logo />
+      </div>
+      <nav>
+        <NavigationItems />
+      </nav>
+    </div>
+    </Aux>
   );
 }
 
